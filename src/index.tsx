62f6ad1d3bf49530af4eba5b02887b9d2d7781bc
Ferran Basora <ferran.basora@factorial.co>
2026-03-03 12:10:26 +0100

    Add global error handlers that point to /tmp/welcome.log on crash

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,19 @@ import React from 'react'
 import { render } from 'ink'
 import App from './App.js'
 
+const LOG_FILE = '/tmp/welcome.log'
+
+process.on('uncaughtException', (err) => {
+  console.error(`\n\nFatal error: ${err.message}`)
+  console.error(`Check ${LOG_FILE} for details.\n`)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (reason) => {
+  const msg = reason instanceof Error ? reason.message : String(reason)
+  console.error(`\n\nUnhandled error: ${msg}`)
+  console.error(`Check ${LOG_FILE} for details.\n`)
+  process.exit(1)
+})
+
 render(<App />)
